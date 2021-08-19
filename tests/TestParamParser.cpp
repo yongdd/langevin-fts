5e1a8efc866598758fd89e120b3c14bb8bc51586
yongdd <mobstersauce@gmail.com>
2021-08-19 12:39:59 +0900

    Basic L_FTS Python Implementation

--- tests/TestParamParser.cpp
+++ tests/TestParamParser.cpp
@@ -15,16 +15,16 @@ int main()
     pp.get("geometry.grids", nx0);
     pp.get("geometry.grids", nx, 3);
     pp.get("chain.chi_n", chi_n);
-    pp.get("filename", filename);
+    pp.get("output.filename", filename);
     as_string = pp.get("chain.chi_n");
 
-    if( nx0 != 24)
+    if( nx0 != 31)
         return -1;
-    if( nx[0] != 24 || nx[1] != 36 || nx[2] != 48)
+    if( nx[0] != 31 || nx[1] != 49 || nx[2] != 63)
         return -1;
     if( (chi_n - 20.0) > 1e-7)
         return -1;
-    if( filename != "asdf")
+    if( filename != "fields")
         return -1;
     if( as_string[0] != "20.0")
         return -1;
@@ -33,7 +33,7 @@ int main()
     std::cout<< "geometry.grids: ";
     std::cout<< nx[0] << " "<< nx[1] << " "<< nx[2] << std::endl;
     std::cout<< "chain.chi_n: "<< chi_n << std::endl;
-    std::cout<< "filename: "<< filename << std::endl;
+    std::cout<< "output.filename: "<< filename << std::endl;
     std::cout<< "as_string: "<< as_string[0] << std::endl;
     
     //pp.display_usage_info();
