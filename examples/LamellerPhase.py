cb4c731ce75e77666d1c02331d0ec6b20ccf5a45
yongdd <mobstersauce@gmail.com>
2021-08-31 02:24:25 +0900

    Reduce Memory Copy in PseudoDiscrete

--- examples/LamellerPhase.py
+++ examples/LamellerPhase.py
@@ -4,8 +4,8 @@
 
 import sys
 import os
-import pathlib
 import time
+import pathlib
 import numpy as np
 from langevinfts import *
 
@@ -20,35 +20,33 @@ def find_saddle_point():
     # saddle point iteration begins here
     for saddle_iter in range(0,saddle_max_iter):
         
-        w[0] = w_plus[:] + w_minus[:]
-        w[1] = w_plus[:] - w_minus[:]
         # for the given fields find the polymer statistics
-        QQ = pseudo.find_phi(phi_a, phi_b, q1_init,q2_init,w[0],w[1])
-        phi_plus[:] = phi_a[:] + phi_b[:]
-        phi_minus[:] = phi_a[:] - phi_b[:]
-        
-        # calculate the total energy
-        energy_old = energy_total
-        energy_total  = -np.log(QQ/sb.get_volume())
-        energy_total += sb.inner_product(w_minus,w_minus)/pc.get_chi_n()/sb.get_volume()
-        energy_total -= sb.integral(w_plus)/sb.get_volume()
+        QQ = pseudo.find_phi(phi_a, phi_b, 
+                q1_init,q2_init,
+                w_plus + w_minus,
+                w_plus - w_minus)
+        phi_plus = phi_a + phi_b
         
         # calculate output fields
-        g_plus = 1.0*(phi_plus[:]-1.0)
-        w_plus_out = w_plus[:] + g_plus[:] 
+        g_plus = 1.0*(phi_plus-1.0)
+        w_plus_out = w_plus + g_plus 
         sb.zero_mean(w_plus_out);
 
         # error_level measures the "relative distance" between the input and output fields
         old_error_level = error_level
         error_level = np.sqrt(sb.inner_product(phi_plus-1.0,phi_plus-1.0)/sb.get_volume())
-        
-        # print iteration # and error levels and check the mass conservation
-        mass_error = (sb.integral(phi_a) + sb.integral(phi_b))/sb.get_volume() - 1.0
 
         # print iteration # and error levels
         if(verbose_level == 2 or
          verbose_level == 1 and
          (error_level < saddle_tolerance or saddle_iter == saddle_max_iter-1 )):
+             
+            # calculate the total energy
+            energy_old = energy_total
+            energy_total  = -np.log(QQ/sb.get_volume())
+            energy_total += sb.inner_product(w_minus,w_minus)/pc.get_chi_n()/sb.get_volume()
+            energy_total -= sb.integral(w_plus)/sb.get_volume()
+
             # check the mass conservation
             mass_error = sb.integral(phi_plus)/sb.get_volume() - 1.0
             print("%8d %12.3E %15.7E %13.9f %13.9f" %
@@ -60,6 +58,7 @@ def find_saddle_point():
         # calculte new fields using simple and Anderson mixing
         # (Caution! we are now passing entire w, w_out and w_diff not just w[0], w_out[0] and w_diff[0])
         am.caculate_new_fields(w_plus, w_plus_out, g_plus, old_error_level, error_level);
+        
 # -------------- simulation parameters ------------
 
 # OpenMP environment variables
@@ -114,11 +113,14 @@ am = factory.create_anderson_mixing(sb, am_n_comp,
 # standard deviation of normal noise for single segment
 langevin_sigma = np.sqrt(2*langevin_dt*sb.get_MM()/ 
     (sb.get_volume()*np.sqrt(langevin_nbar)))
-rg = RandomGaussian()
+    
+# random seed for MT19937
+np.random.seed(5489);  
+print("Random Number Generator: ", np.random.RandomState().get_state()[0])
 
 # -------------- print simulation parameters ------------
 print("---------- Simulation Parameters ----------");
-print("Box Dimension: 3")
+print("Box Dimension: %d"  % (sb.get_dimension()) )
 print("Precision: 8")
 print("chi_n: %f, f: %f, NN: %d" % (pc.get_chi_n(), pc.get_f(), pc.get_NN()) )
 print("Nx: %d, %d, %d" % (sb.get_nx(0), sb.get_nx(1), sb.get_nx(2)) )
@@ -127,26 +129,18 @@ print("dx: %f, %f, %f" % (sb.get_dx(0), sb.get_dx(1), sb.get_dx(2)) )
 print("Volume: %f" % (sb.get_volume()) )
 
 print("Invariant Polymerization Index: %d" % (langevin_nbar) )
+print("Langevin Sigma: %f" % (langevin_sigma) )
+print("Random Number Generator: ", np.random.RandomState().get_state()[0])
+
 #-------------- allocate array ------------
-w            = np.zeros([2, sb.get_MM()], dtype=np.float64)
-w_out        = np.zeros([2, sb.get_MM()], dtype=np.float64)
-w_diff       = np.zeros([2, sb.get_MM()], dtype=np.float64)
-w_plus       = np.zeros(    sb.get_MM(),  dtype=np.float64)
-w_minus      = np.zeros(    sb.get_MM(),  dtype=np.float64)
-w_minus_copy = np.zeros(    sb.get_MM(),  dtype=np.float64)
-normal_noise = np.zeros(    sb.get_MM(),  dtype=np.float64)
 q1_init      = np.zeros(    sb.get_MM(),  dtype=np.float64)
 q2_init      = np.zeros(    sb.get_MM(),  dtype=np.float64)
 phi_a        = np.zeros(    sb.get_MM(),  dtype=np.float64)
 phi_b        = np.zeros(    sb.get_MM(),  dtype=np.float64)
-phi_plus     = np.zeros(    sb.get_MM(),  dtype=np.float64)
-phi_minus    = np.zeros(    sb.get_MM(),  dtype=np.float64)
 
 print("wminus and wplus are initialized to random")
-for i in range(0,sb.get_MM()):
-    w_plus[i] = rg.normal_dist(0, langevin_sigma)
-for i in range(0,sb.get_MM()):
-    w_minus[i] = rg.normal_dist(0, langevin_sigma)
+w_plus  = np.random.normal(0.0, langevin_sigma, sb.get_MM())
+w_minus = np.random.normal(0.0, langevin_sigma, sb.get_MM())
 
 # keep the level of field value
 sb.zero_mean(w_plus);
@@ -161,22 +155,22 @@ find_saddle_point()
 #------------------ run ----------------------
 print("---------- Run ----------")
 time_start = time.time()
+
 #print("iteration, mass error, total_partition, energy_total, error_level")
 for langevin_step in range(0, langevin_max_iter):
     
     print("langevin step: ", langevin_step)
     # update w_minus: predict step
-    w_minus_copy[:] = w_minus[:]
-    for i in range(0,sb.get_MM()):
-        normal_noise[i] = rg.normal_dist(0, langevin_sigma)
-    lambda1 = phi_a[:]-phi_b[:] + 2*w_minus[:]/pc.get_chi_n()
-    w_minus[:] += -lambda1[:]*langevin_dt + normal_noise[:]
+    w_minus_copy = w_minus.copy()
+    normal_noise = np.random.normal(0.0, langevin_sigma, sb.get_MM())
+    lambda1 = phi_a-phi_b + 2*w_minus/pc.get_chi_n()
+    w_minus += -lambda1*langevin_dt + normal_noise
     sb.zero_mean(w_minus)
     find_saddle_point()
     
     # update w_minus: correct step 
-    lambda2 = phi_a[:]-phi_b[:] + 2*w_minus[:]/pc.get_chi_n()
-    w_minus[:] = w_minus_copy[:] - 0.5*(lambda1[:]+lambda2[:])*langevin_dt + normal_noise[:]
+    lambda2 = phi_a-phi_b + 2*w_minus/pc.get_chi_n()
+    w_minus = w_minus_copy - 0.5*(lambda1+lambda2)*langevin_dt + normal_noise
     sb.zero_mean(w_minus)
     find_saddle_point()
 
@@ -184,9 +178,11 @@ for langevin_step in range(0, langevin_max_iter):
         np.savez("data/fields_%06d.npz" % (langevin_step),
         nx=nx, lx=lx, N=NN, f=pc.get_f(), chi_n=pc.get_chi_n(),
         polymer_model=polymer_model, n_bar=langevin_nbar,
+        random_seed=np.random.RandomState().get_state()[0],
         w_minus=w_minus, w_plus=w_plus)
 
 # estimate execution time
 time_duration = time.time() - time_start; 
 print( "total time: %f, time per step: %f" %
     (time_duration, time_duration/langevin_max_iter) )
+
