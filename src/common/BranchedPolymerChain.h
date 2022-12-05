3e505afdcfd8e287411e09af0e4150ac224a11a6
yongdd <yongdd@kias.re.kr>
2022-12-06 02:58:48 +0900

    under development: branched polymer

--- src/common/BranchedPolymerChain.h
+++ src/common/BranchedPolymerChain.h
@@ -9,6 +9,17 @@
 #include <vector>
 #include <map>
 
+struct polymer_chain_edge_data{
+    std::string species;        // block species
+    std::string dependency;     // prerequisite partial partition functions as a text
+    int n_segment;              // the number of segments
+};
+
+struct polymer_chain_optimal_sub_branches_data{
+    std::vector<std::pair<std::string, int>> dependency;  // prerequisite partial partition functions as texts
+    int max_segment;                                        // the maximum segment length for given branches
+};
+
 class BranchedPolymerChain
 {
 private:
@@ -20,26 +31,21 @@ private:
     // <node, node2, species, block_length>
     // e.g., <0, 1, "A", 0.5>
     //std::vector<std::map<int, int, std::string, double>> blocks;
-
-    // int n_block;         // total number of blocks
-    // int n_segment_total; // total number of segments, N_p
     std::vector<std::string> block_types;         // sequence of block types, e.g., ["A","B",...]
     std::vector<int> n_segments;            // sequence of block segments number, e.g., [N_A, N_B, ...]
     std::vector<double> bond_length_sq;     // square of sequence of statistical_segment_length [(a_A/a_Ref)^2, (a_B/a_Ref)^2, ...]
 
     //std::vector<int> v_edges, u_edges;             // v and u of edge
-    std::map<int, std::vector<int>> adjacent_nodes;        // 
-    std::map<std::pair<int, int>, std::pair<std::string, int>> edges;  // 
-    std::map<std::string, std::vector<std::pair<std::string, int>>, std::greater<std::string>> dependencies;
-    std::map<std::string, int, std::greater<std::string>> max_segments;
+    std::map<int, std::vector<int>> adjacent_nodes;                     // 
+    std::map<std::pair<int, int>, polymer_chain_edge_data> edges;   // species, segments and dependency for each edge
 
-    // std::vector<int> block_start;   // index of starting segment of each block
-                                       // [0,N_A,N_A+N_B,...,N] (length = n_block+1)
-    // std::vector<int> block_lengths;     // sequence of block lengths, e.g., [0.3, 0.4, ...]
+    // non-duplicated optimal sub_branches
+    std::map<std::string, polymer_chain_optimal_sub_branches_data, std::greater<std::string>> optimal_sub_branches; 
 
+    // get sub-branch information as ordered texts
     std::pair<std::string, int> get_text_of_ordered_branches(int in_node, int out_node);
-
 public:
+
     BranchedPolymerChain(std::string model_name, double ds, std::map<std::string, double> dict_segment_lengths,
         std::vector<std::string> block_types, std::vector<double> block_lengths, std::vector<int> v, std::vector<int> u);
     ~BranchedPolymerChain() {};
@@ -52,11 +58,14 @@ public:
     int get_n_block();
     std::vector<int> get_n_segment();    // [N_A, N_B, ...]
     int get_n_segment(int block);
-    //int get_n_segment_total();
 
     std::vector<std::string> get_block_type();
     std::string get_block_type(int block_number);
 
-    //std::vector<int> get_block_start();
+    // get information of optimal sub_graph
+    std::map<std::string, polymer_chain_optimal_sub_branches_data, std::greater<std::string>> get_optimal_sub_branches(); 
+    int get_n_sub_graph();
+    std::vector<std::pair<std::string, int>> get_dependency(std::string key);
+    int get_max_segment(std::string key);
 };
 #endif
---
+++ src/platforms/cpu/CpuPseudoBranchedContinuous.h
@@ -0,0 +1,35 @@
+/*-------------------------------------------------------------
+* This is an abstract CpuPseudoContinuous class
+*------------------------------------------------------------*/
+
+#ifndef CPU_PSEUDO_BRANCHED_CONTINUOUS_H_
+#define CPU_PSEUDO_BRANCHED_CONTINUOUS_H_
+
+#include "ComputationBox.h"
+#include "BranchedPolymerChain.h"
+#include "Pseudo.h"
+#include "FFT.h"
+
+class CpuPseudoBranchedContinuous : public Pseudo
+{
+private:
+    FFT *fft;
+    double *q_1, *q_2;
+    double **boltz_bond;
+    double **boltz_bond_half;
+    void one_step(double *q_in, double *q_out, 
+                  double *boltz_bond, double *boltz_bond_half,
+                  double *exp_dw, double *exp_dw_half);
+    void calculate_phi_one_type(double *phi, const int N_START, const int N_END);
+    void init_simpson_rule_coeff(double *coeff, const int N);
+public:
+    CpuPseudoBranchedContinuous(ComputationBox *cb, PolymerChain *pc, FFT *ff);
+    ~CpuPseudoBranchedContinuous();
+    
+    void update() override;
+    std::array<double,3> dq_dl() override;
+    void compute_statistics(double *phi, double *q_1_init, double *q_2_init,
+                    std::map<std::string, double*> w_block, double &single_partition) override;
+    void get_partition(double *q_1_out, int n1, double *q_2_out, int n2) override;
+};
+#endif
