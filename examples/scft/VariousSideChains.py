9a617529427580d77375b551c79980a27e43b651
yongdd <yongdd@kias.re.kr>
2023-02-11 00:32:01 +0900

    fixing bug: cuda continuous, copy nonzero segment

--- examples/scft/VariousSideChains.py
+++ examples/scft/VariousSideChains.py
@@ -24,7 +24,7 @@ params = {
                                 # To obtain concentration of each block, disable this option.
 
     "box_is_altering":True,       # Find box size that minimizes the free energy during saddle point iteration.
-    "chain_model":"discrete",   # "discrete" or "continuous" chain model
+    "chain_model":"continuous",   # "discrete" or "continuous" chain model
     "ds":1/10,                    # Contour step interval, which is equal to 1/N_Ref.
     "chi_n": 15.0,                # Interaction parameter, Flory-Huggins params * N
 
@@ -41,7 +41,7 @@ params = {
             {"type":"A", "length":f,   "v":3, "u":4},    # A-block
             {"type":"A", "length":f,   "v":4, "u":5},    # A-block
 
-            {"type":"B", "length":(1-f)/1, "v":1, "u":6},    # B-block
+            {"type":"B", "length":1-f, "v":1, "u":6},    # B-block
             {"type":"B", "length":1-f, "v":2, "u":7},    # B-block
             {"type":"B", "length":(1-f)/2, "v":3, "u":8},    # B-block
             {"type":"B", "length":(1-f)/4, "v":4, "u":9},    # B-block
@@ -53,7 +53,7 @@ params = {
             {"type":"A", "length":f, "v":7, "u":13},    # B-block
         ],},],
 
-    "max_iter":1,      # The maximum relaxation iterations
+    "max_iter":5,      # The maximum relaxation iterations
     "tolerance":1e-8,     # Terminate iteration if the self-consistency error is less than tolerance
 
     "am":{
@@ -105,10 +105,16 @@ mdic = {"params":params, "dim":len(params["nx"]), "nx":params["nx"], "lx":params
         "w_a":w_A, "w_b":w_B, "phi_a":phi_A, "phi_b":phi_B}
 savemat("fields.mat", mdic)
 
-# Recording first a few iteration results for debugging and refactoring
-    # 1    3.553E-15  [ 2.5612154E+01  ]    -0.002751834   1.1477009E+00  [  2.9000000, 2.9000000, 2.9000000 ]
-    # 2   -1.199E-14  [ 2.5579319E+01  ]    -0.002374078   1.0665875E+00  [  2.9000198, 2.9000198, 2.9000198 ]
-    # 3    7.327E-15  [ 2.5551103E+01  ]    -0.002039748   9.9023430E-01  [  2.9000380, 2.9000380, 2.9000380 ]
-    # 4    2.132E-14  [ 2.5526964E+01  ]    -0.001744373   9.1850175E-01  [  2.9000550, 2.9000550, 2.9000550 ]
-    # 5    3.109E-15  [ 2.5506412E+01  ]    -0.001483872   8.5122439E-01  [  2.9000707, 2.9000707, 2.9000707 ]
-    # 6    6.217E-15  [ 2.5489005E+01  ]    -0.001254531   7.8821910E-01  [  2.9000854, 2.9000854, 2.9000854 ]
+# Recording first a few iteration results for debugging and refactoring (continuous)
+    #    1    4.263E-14  [ 1.0587717E+00  ]    -0.003539667   1.3084945E+00  [  2.9000000, 2.9000000, 2.9000000 ]
+    #    2   -1.665E-14  [ 1.0569722E+00  ]    -0.003075021   1.2181248E+00  [  2.9000242, 2.9000242, 2.9000242 ]
+    #    3   -7.772E-16  [ 1.0554140E+00  ]    -0.002660513   1.1328753E+00  [  2.9000466, 2.9000466, 2.9000466 ]
+    #    4    3.331E-15  [ 1.0540702E+00  ]    -0.002291366   1.0526313E+00  [  2.9000673, 2.9000673, 2.9000673 ]
+    #    5   -2.720E-14  [ 1.0529165E+00  ]    -0.001963178   9.7723954E-01  [  2.9000865, 2.9000865, 2.9000865 ]
+
+# Recording first a few iteration results for debugging and refactoring (discrete)
+    #    1    1.288E-14  [ 1.0624381E+00  ]    -0.004204454   1.3573602E+00  [  2.9000000, 2.9000000, 2.9000000 ]
+    #    2    1.554E-15  [ 1.0608201E+00  ]    -0.003746740   1.2644473E+00  [  2.9000216, 2.9000216, 2.9000216 ]
+    #    3    6.217E-15  [ 1.0594495E+00  ]    -0.003339259   1.1769938E+00  [  2.9000414, 2.9000414, 2.9000414 ]
+    #    4   -2.021E-14  [ 1.0582986E+00  ]    -0.002977130   1.0948653E+00  [  2.9000595, 2.9000595, 2.9000595 ]
+    #    5   -1.155E-14  [ 1.0573425E+00  ]    -0.002655865   1.0178862E+00  [  2.9000762, 2.9000762, 2.9000762 ]
