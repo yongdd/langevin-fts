36de95051e0f1fa8a82d0290de46d674acafe415
yongdd <yongdd@kias.re.kr>
2024-08-29 05:16:41 +0900

    variants of symmetric multi-monomer theory

---
+++ devel/SymmetricTheory/Lamella.py
@@ -0,0 +1,108 @@
+import os
+import time
+import numpy as np
+from scipy.io import savemat, loadmat
+from scipy.ndimage import gaussian_filter
+import lfts
+
+# OpenMP environment variables
+os.environ["OMP_MAX_ACTIVE_LEVELS"] = "1"  # 0, 1
+os.environ["OMP_NUM_THREADS"] = "2"  # 1 ~ 4
+
+f = 0.5         # A-fraction of major BCP chain, f
+eps = 1.0       # a_A/a_B, conformational asymmetry
+
+params = {
+    # "platform":"cuda",           # choose platform among [cuda, cpu-mkl]
+
+    "nx":[32, 32, 32],          # Simulation grid numbers
+    "lx":[8.0, 8.0, 8.0],       # Simulation box size as a_Ref * N_Ref^(1/2) unit,
+                                # where "a_Ref" is reference statistical segment length
+                                # and "N_Ref" is the number of segments of reference linear homopolymer chain.
+
+    "chain_model":"continuous", # "discrete" or "continuous" chain model
+    "ds":1/16,                  # Contour step interval, which is equal to 1/N_Ref.
+
+    "segment_lengths":{         # Relative statistical segment length compared to "a_Ref.
+        "A":np.sqrt(eps*eps/(eps*eps*f + (1-f))), 
+        "B":np.sqrt(    1.0/(eps*eps*f + (1-f))), },
+
+    "chi_n": {"A,B": 20.0},  # Bare interaction parameter, Flory-Huggins params * N_Ref
+
+    "distinct_polymers":[{      # Distinct Polymers
+        "volume_fraction":1.0,  # Volume fraction of polymer chain
+        "blocks":[              # AB diBlock Copolymer
+            {"type":"A", "length":f, }, # A-block
+            {"type":"B", "length":1-f}, # B-block
+        ],},],
+        
+    "langevin":{                # Langevin Dynamics
+        "max_step":50000,       # Langevin steps for simulation
+        "dt":8.0,               # Langevin step interval, delta tau*N_Ref
+        "nbar":1024,            # Invariant polymerization index, nbar of N_Ref
+    },
+    
+    "recording":{                       # Recording Simulation Data
+        "dir":"data_simulation",        # Directory name
+        "recording_period":1000,        # Period for recording concentrations and fields
+        "sf_computing_period":10,       # Period for computing structure function
+        "sf_recording_period":1000,    # Period for recording structure function
+    },
+
+    "saddle":{                # Iteration for the pressure field 
+        "max_iter" :100,      # Maximum number of iterations
+        "tolerance":1e-4,     # Tolerance of incompressibility 
+    },
+
+    "am":{
+        "max_hist":20,              # Maximum number of history
+        "start_error":5e-1,         # When switch to AM from simple mixing
+        "mix_min":0.01,             # Minimum mixing rate of simple mixing
+        "mix_init":0.01,            # Initial mixing rate of simple mixing
+    },
+
+    "verbose_level":1,      # 1 : Print at each Langevin step.
+                            # 2 : Print at each saddle point iteration.
+}
+# Set random seed
+# If you want to obtain different results for each execution, set random_seed=None
+random_seed = 12345
+np.random.seed(random_seed)
+
+# Set initial fields
+print("w_A and w_B are initialized to random Gaussian.")
+w_A = np.random.normal(0.0, 1.0, params["nx"])
+w_B = np.random.normal(0.0, 1.0, params["nx"])
+
+# Initialize calculation
+simulation = lfts.LFTS(params=params, random_seed=random_seed)
+
+# Set a timer
+time_start = time.time()
+
+# Run
+simulation.run(initial_fields={"A": w_A, "B": w_B})
+
+# # Continue simulation with recorded field configurations and random state.
+# simulation.continue_run(file_name="fields_010000.mat")
+
+# Estimate execution time
+time_duration = time.time() - time_start
+print("total time: %f, time per step: %f" %
+    (time_duration, time_duration/params["langevin"]["max_step"]) )
+
+# Recording first a few iteration results for debugging and refactoring
+
+#       20    1.571E-16  [ 1.0217261E+00  ]     5.004521219   8.9464276E-05 
+# iteration, mass error, total partitions, total energy, incompressibility error
+# ---------- Run  ----------
+# Langevin step:  1
+#       22    2.280E-16  [ 1.1036824E+00  ]     5.021894299   [9.5673754E-05 ]
+# Langevin step:  2
+#       24    5.243E-17  [ 1.4700034E+00  ]     5.099646978   [8.0163485E-05 ]
+# Langevin step:  3
+#       24    1.736E-16  [ 1.8943395E+00  ]     5.170455712   [9.4283904E-05 ]
+# Langevin step:  4
+#       26   -1.560E-16  [ 2.3857432E+00  ]     5.234860548   [7.0143591E-05 ]
+# Langevin step:  5
+#       25   -4.339E-16  [ 2.9502444E+00  ]     5.290531514   [7.3829234E-05 ]
