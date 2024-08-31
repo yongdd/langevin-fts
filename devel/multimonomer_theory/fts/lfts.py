bug fix: wrong the sign of dH/dW

devel/multimonomer_theory/fts/lfts.py
@@ -11,10 +11,11 @@ from scipy.io import savemat, loadmat
 from langevinfts import *
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from mpt_mse import *
-from mpt_original import *
-from mpt_traditional import *
-from mpt_orthonormal import *
+from models.mpt_mse import *
+from models.mpt_mse_no_const_term import *
+from models.mpt_original import *
+from models.mpt_traditional import *
+from models.mpt_orthonormal import *
 
 # OpenMP environment variables
 os.environ["MKL_NUM_THREADS"] = "1"  # always 1
@@ -79,8 +80,8 @@ class LFTS:
         # Multi-monomer polymer field theory
         # self.mpt = MPT_MSE(self.monomer_types, self.chi_n)
         # self.mpt = MPT_Original(self.monomer_types, self.chi_n)
-        # self.mpt = MPT_Traditional(self.monomer_types, self.chi_n)
-        self.mpt = MPT_Orthonormal(self.monomer_types, self.chi_n)
+        self.mpt = MPT_Traditional(self.monomer_types, self.chi_n)
+        # self.mpt = MPT_Orthonormal(self.monomer_types, self.chi_n)
         
         # Total volume fraction
         assert(len(params["distinct_polymers"]) >= 1), \
@@ -618,7 +619,7 @@ class LFTS:
                 h_deriv[count] *= self.dt_scaling[i]
 
             # Calculate new fields using simple and Anderson mixing
-            w_eigen[self.mpt.eigen_fields_imag_idx] = np.reshape(self.am.calculate_new_fields(w_eigen[self.mpt.eigen_fields_imag_idx], h_deriv, old_error_level, error_level), [I, self.cb.get_n_grid()])
+            w_eigen[self.mpt.eigen_fields_imag_idx] = np.reshape(self.am.calculate_new_fields(w_eigen[self.mpt.eigen_fields_imag_idx], -h_deriv, old_error_level, error_level), [I, self.cb.get_n_grid()])
         
         # Set mean of pressure field to zero
         w_eigen[S-1] -= np.mean(w_eigen[S-1])

devel/multimonomer_theory/models/mpt_mse.py
@@ -20,18 +20,6 @@ class MPT_MSE:
             vector_s[i] = chi_n[key]
         self.vector_large_s = np.matmul(np.transpose(matrix_o), vector_s)
 
-        # self.matrix_chi = matrix_chi
-        # self.vector_s = np.matmul(matrix_chi, np.ones(S))/S
-        # self.vector_large_s = np.matmul(np.transpose(matrix_o), self.vector_s)
-
-        vector_large_s_prime = self.vector_large_s.copy()
-        for i in range(S-1):
-            if not np.isclose(eigenvalues[i], 0.0):
-                vector_large_s_prime[i] = 0.0
-        o_large_s = np.zeros(S)
-        o_large_s[0:S-1] = np.matmul(matrix_o, vector_large_s_prime)
-        self.o_large_s = np.reshape(o_large_s, (S, 1))
-
         # Indices whose eigen fields are real
         self.eigen_fields_real_idx = []
         # Indices whose eigen fields are imaginary including the pressure field
@@ -122,8 +110,8 @@ class MPT_MSE:
         print("\tcoefficients of int of mu(r)/V: ", self.h_coef_mu1)
         print("\tcoefficients of int of mu(r)^2/V: ", self.h_coef_mu2)
         print("\tdH_ref/dχN: ", self.h_const_deriv_chin)
-        print("\td(coef of mu1)/dχN: ", self.h_coef_mu1_deriv_chin)
-        print("\td(coef of mu2)/dχN: ", self.h_coef_mu2_deriv_chin)
+        print("\td(coef of mu(r))/dχN: ", self.h_coef_mu1_deriv_chin)
+        print("\td(coef of mu(r)^2)/dχN: ", self.h_coef_mu2_deriv_chin)
 
     def to_eigen_fields(self, w):
         return np.matmul(self.matrix_a_inv, w)
@@ -212,12 +200,11 @@ class MPT_MSE:
                 h_deriv[count] +=   self.h_coef_mu1[i]
                 for j in range(S):
                     h_deriv[count] += self.matrix_a[j,i]*phi[self.monomer_types[j]]
-                
-            # Pressure fields
+            # Pressure field
             else:
                 for j in range(S):
-                    h_deriv[count] += phi[self.monomer_types[j]]
-                h_deriv[count] -= 1.0
+                    h_deriv[count] -= phi[self.monomer_types[j]]
+                h_deriv[count] += 1.0
         elapsed_time["h_deriv"] = time.time() - time_e_start
         
         return  h_deriv, elapsed_time

devel/multimonomer_theory/models/mpt_mse_no_const_term.py
@@ -1,31 +1,27 @@
 import time
 import numpy as np
 
-class MPT_Orthonormal:
+
+class MPT_MSE_No_Const:
     def __init__(self, monomer_types, chi_n):
         self.monomer_types = monomer_types
         S = len(self.monomer_types)
-        
-        self.matrix_q = np.ones((S,S))/S
-        self.matrix_p = np.identity(S) - self.matrix_q
-        
+
         # Compute eigenvalues and orthogonal matrix
-        eigenvalues, matrix_o = self.compute_eigen_system(chi_n, self.matrix_p)
+        eigenvalues, matrix_o = self.compute_eigen_system(chi_n)
 
-        # Construct chi_n matrix
-        matrix_chi = np.zeros((S,S))
-        for i in range(S):
-            for j in range(i+1,S):
-                monomer_pair = [self.monomer_types[i], self.monomer_types[j]]
-                monomer_pair.sort()
-                key = monomer_pair[0] + "," + monomer_pair[1]
-                if key in chi_n:
-                    matrix_chi[i,j] = chi_n[key]
-                    matrix_chi[j,i] = chi_n[key]
+        # Compute vector X_iS
+        vector_s = np.zeros(S-1)
+        for i in range(S-1):
+            monomer_pair = [self.monomer_types[i], self.monomer_types[S-1]]
+            monomer_pair.sort()
+            key = monomer_pair[0] + "," + monomer_pair[1]            
+            vector_s[i] = chi_n[key]
+        self.vector_large_s = np.matmul(np.transpose(matrix_o), vector_s)
 
-        self.matrix_chi = matrix_chi
-        self.vector_s = np.matmul(matrix_chi, np.ones(S))/S
-        self.vector_large_s = np.matmul(np.transpose(matrix_o), self.vector_s)
+        self.o_large_s = np.zeros(S)
+        self.o_large_s[0:S-1] = vector_s
+        self.o_large_s = np.reshape(self.o_large_s, (S, 1))
 
         # Indices whose eigen fields are real
         self.eigen_fields_real_idx = []
@@ -51,14 +47,17 @@ class MPT_Orthonormal:
             print("The field fluctuations would not be fully reflected. Run this simulation at your own risk.")
 
         # Compute coefficients for Hamiltonian computation
-        h_const, h_coef_mu1, h_coef_mu2 = self.compute_h_coef(chi_n, eigenvalues)
+        h_const, h_coef_mu1, h_coef_mu2 = self.compute_h_coef(eigenvalues)
 
         # Matrix A and Inverse for converting between eigen fields and species chemical potential fields
         matrix_a = np.zeros((S,S))
-        matrix_a[:,0:S-1] = matrix_o[:,0:S-1]
-        matrix_a[:,S-1] = 1.0
-        matrix_a_inv = np.transpose(matrix_a.copy())
-        matrix_a_inv[S-1,:] = 1.0/S
+        matrix_a_inv = np.zeros((S,S))
+        matrix_a[0:S-1,0:S-1] = matrix_o[0:S-1,0:S-1]
+        matrix_a[:,S-1] = 1
+        matrix_a_inv[0:S-1,0:S-1] = np.transpose(matrix_o[0:S-1,0:S-1])
+        for i in range(S-1):
+            matrix_a_inv[i,S-1] =  -np.sum(matrix_o[:,i])
+            matrix_a_inv[S-1,S-1] = 1
 
         # Check the inverse matrix
         error = np.std(np.matmul(matrix_a, matrix_a_inv) - np.identity(S))
@@ -79,12 +78,12 @@ class MPT_Orthonormal:
             chi_n_n[key] -= epsilon
 
             # Compute eigenvalues and orthogonal matrix
-            eigenvalues_p, _ = self.compute_eigen_system(chi_n_p, self.matrix_p)
-            eigenvalues_n, _ = self.compute_eigen_system(chi_n_n, self.matrix_p)
+            eigenvalues_p, _ = self.compute_eigen_system(chi_n_p)
+            eigenvalues_n, _ = self.compute_eigen_system(chi_n_n)
             
             # Compute coefficients for Hamiltonian computation
-            h_const_p, h_coef_mu1_p, h_coef_mu2_p = self.compute_h_coef(chi_n_p, eigenvalues_p)
-            h_const_n, h_coef_mu1_n, h_coef_mu2_n = self.compute_h_coef(chi_n_n, eigenvalues_n)
+            h_const_p, h_coef_mu1_p, h_coef_mu2_p = self.compute_h_coef(eigenvalues_p)
+            h_const_n, h_coef_mu1_n, h_coef_mu2_n = self.compute_h_coef(eigenvalues_n)
             
             # Compute derivatives using finite difference
             self.h_const_deriv_chin[key] = (h_const_p - h_const_n)/(2*epsilon)
@@ -100,8 +99,6 @@ class MPT_Orthonormal:
         self.matrix_a = matrix_a
         self.matrix_a_inv = matrix_a_inv
 
-        # print("Projection matrix P:\n\t", str(self.matrix_p).replace("\n", "\n\t"))
-        # print("Projection matrix Q:\n\t", str(self.matrix_q).replace("\n", "\n\t"))
         print("Eigenvalues:\n\t", self.eigenvalues)
         print("Eigenvectors [v1, v2, ...] :\n\t", str(self.matrix_o).replace("\n", "\n\t"))
         print("Mapping matrix A:\n\t", str(self.matrix_a).replace("\n", "\n\t"))
@@ -116,19 +113,19 @@ class MPT_Orthonormal:
         print("\tcoefficients of int of mu(r)/V: ", self.h_coef_mu1)
         print("\tcoefficients of int of mu(r)^2/V: ", self.h_coef_mu2)
         print("\tdH_ref/dχN: ", self.h_const_deriv_chin)
-        print("\td(coef of mu1)/dχN: ", self.h_coef_mu1_deriv_chin)
-        print("\td(coef of mu2)/dχN: ", self.h_coef_mu2_deriv_chin)
+        print("\td(coef of mu(r))/dχN: ", self.h_coef_mu1_deriv_chin)
+        print("\td(coef of mu(r)^2)/dχN: ", self.h_coef_mu2_deriv_chin)
 
     def to_eigen_fields(self, w):
-        return np.matmul(self.matrix_a_inv, w)
+        return np.matmul(self.matrix_a_inv, w-self.o_large_s)
 
     def to_monomer_fields(self, w_eigen):
-        return np.matmul(self.matrix_a, w_eigen)
+        return np.matmul(self.matrix_a, w_eigen) + self.o_large_s
 
-    def compute_eigen_system(self, chi_n, matrix_p):
-        S = matrix_p.shape[0]
-
+    def compute_eigen_system(self, chi_n):
+        S = len(self.monomer_types)
         matrix_chi = np.zeros((S,S))
+        matrix_chin = np.zeros((S-1,S-1))
         for i in range(S):
             for j in range(i+1,S):
                 monomer_pair = [self.monomer_types[i], self.monomer_types[j]]
@@ -137,66 +134,27 @@ class MPT_Orthonormal:
                 if key in chi_n:
                     matrix_chi[i,j] = chi_n[key]
                     matrix_chi[j,i] = chi_n[key]
-                    
-        projected_chin = np.matmul(matrix_p, np.matmul(matrix_chi, matrix_p))
-
-        eigenvalues, eigenvectors = np.linalg.eigh(projected_chin)
-        sorted_indexes = np.argsort(np.abs(eigenvalues))[::-1]
-        eigenvalues = eigenvalues[sorted_indexes]
-        eigenvectors = eigenvectors[:,sorted_indexes]
-
-        # Set the last eigenvector to np.ones(S)/np.sqrt(S)
-        eigenvectors[:,-1] = np.ones(S)/np.sqrt(S)
         
-        # Make a orthogonal matrix using Gram-Schmidt
-        eigen_val_0 = np.isclose(eigenvalues, 0.0, atol=1e-12)
-        eigenvalues[eigen_val_0] = 0.0
-        eigen_vec_0 = eigenvectors[:,eigen_val_0]
-        for i in range(eigen_vec_0.shape[1]-2,-1,-1):
-            vec_0 = eigen_vec_0[:,i].copy()
-            for j in range(i+1, eigen_vec_0.shape[1]):
-                eigen_vec_0[:,i] -= eigen_vec_0[:,j]*np.dot(vec_0,eigen_vec_0[:,j])
-            eigen_vec_0[:,i] /= np.linalg.norm(eigen_vec_0[:,i])
-        eigenvectors[:,eigen_val_0] = eigen_vec_0
+        for i in range(S-1):
+            for j in range(S-1):
+                matrix_chin[i,j] = matrix_chi[i,j] - matrix_chi[i,S-1] - matrix_chi[j,S-1] # fix a typo in the paper
+        
+        return np.linalg.eigh(matrix_chin)
 
-        # Make the first element of each vector positive to recover the conventional AB polymer field theory
-        for i in range(S):
-            if eigenvectors[0,i] < 0.0:
-                eigenvectors[:,i] *= -1.0
-
-        # # Multiply np.sqrt(S) to eigenvectors
-        # eigenvectors *= np.sqrt(S)
-
-        return eigenvalues, eigenvectors
-
-    def compute_h_coef(self, chi_n, eigenvalues):
+    def compute_h_coef(self, eigenvalues):
         S = len(self.monomer_types)
 
-        # Compute vector X_iS
-        vector_s = np.zeros(S-1)
-        for i in range(S-1):
-            monomer_pair = [self.monomer_types[i], self.monomer_types[S-1]]
-            monomer_pair.sort()
-            key = monomer_pair[0] + "," + monomer_pair[1]            
-            vector_s[i] = chi_n[key]
-
         # Compute reference part of Hamiltonian
-        h_const = 0.5*np.sum(self.vector_s)/S
-        for i in range(S-1):
-            if not np.isclose(eigenvalues[i], 0.0):
-                h_const -= 0.5*self.vector_large_s[i]**2/eigenvalues[i]#/S
+        h_const = 0.0
 
         # Compute coefficients of integral of μ(r)/V
         h_coef_mu1 = np.zeros(S-1)
-        for i in range(S-1):
-            if not np.isclose(eigenvalues[i], 0.0):
-                h_coef_mu1[i] = self.vector_large_s[i]/eigenvalues[i]
 
         # Compute coefficients of integral of μ(r)^2/V
         h_coef_mu2 = np.zeros(S-1)
         for i in range(S-1):
             if not np.isclose(eigenvalues[i], 0.0):
-                h_coef_mu2[i] = -0.5/eigenvalues[i]#*S
+                h_coef_mu2[i] = -0.5/eigenvalues[i]
 
         return h_const, h_coef_mu1, h_coef_mu2
 
@@ -233,12 +191,11 @@ class MPT_Orthonormal:
                 h_deriv[count] +=   self.h_coef_mu1[i]
                 for j in range(S):
                     h_deriv[count] += self.matrix_a[j,i]*phi[self.monomer_types[j]]
-                
-            # Pressure fields
+            # Pressure field
             else:
                 for j in range(S):
-                    h_deriv[count] += phi[self.monomer_types[j]]
-                h_deriv[count] -= 1.0
+                    h_deriv[count] -= phi[self.monomer_types[j]]
+                h_deriv[count] += 1.0
         elapsed_time["h_deriv"] = time.time() - time_e_start
         
         return  h_deriv, elapsed_time

devel/multimonomer_theory/scft/Lamella3D.py
@@ -1,7 +1,7 @@
 import os
 import time
 import numpy as np
-from scipy.io import savemat
+from scipy.io import savemat, loadmat
 import scft
 
 # OpenMP environment variables
@@ -44,7 +44,7 @@ params = {
         "mix_init":0.1,         # Initial mixing rate of simple mixing
     },
 
-    "max_iter":2,     # The maximum relaxation iterations
+    "max_iter":2000,     # The maximum relaxation iterations
     "tolerance":1e-8     # Terminate iteration if the self-consistency error is less than tolerance
 }
 
@@ -53,8 +53,8 @@ w_A = np.zeros(list(params["nx"]), dtype=np.float64)
 w_B = np.zeros(list(params["nx"]), dtype=np.float64)
 print("w_A and w_B are initialized to lamellar phase.")
 for i in range(0,params["nx"][2]):
-    w_A[:,:,i] =  np.cos(3*2*np.pi*i/params["nx"][2])
-    w_B[:,:,i] = -np.cos(3*2*np.pi*i/params["nx"][2])
+    w_A[:,:,i] =  5*np.cos(3*2*np.pi*i/params["nx"][2])
+    w_B[:,:,i] = -5*np.cos(3*2*np.pi*i/params["nx"][2])
 
 # Initialize calculation
 calculation = scft.SCFT(params=params)
@@ -71,11 +71,3 @@ print("total time: %f " % time_duration)
 
 # Save final results
 calculation.save_results("fields.mat")
-
-# Recording first a few iteration results for debugging and refactoring
-    #    1   -4.508E-14  [ 1.0481009E+00  ]    -0.009300846   2.4708108E-01  [  4.3600000, 4.3600000, 4.3600000 ]
-    #    2    5.462E-14  [ 1.0505473E+00  ]    -0.009752572   2.4654500E-01  [  4.3600000, 4.3600000, 4.3597350 ]
-    #    3    1.095E-13  [ 1.0531431E+00  ]    -0.010230798   2.4604978E-01  [  4.3600000, 4.3600000, 4.3594551 ]
-    #    4    4.419E-14  [ 1.0558985E+00  ]    -0.010737036   2.4558510E-01  [  4.3600000, 4.3600000, 4.3591594 ]
-    #    5    1.186E-13  [ 1.0588245E+00  ]    -0.011272911   2.4514220E-01  [  4.3600000, 4.3600000, 4.3588468 ]
-    

devel/multimonomer_theory/scft/scft.py
@@ -10,10 +10,11 @@ from scipy.io import savemat, loadmat
 from langevinfts import *
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from mpt_mse import *
-from mpt_original import *
-from mpt_traditional import *
-from mpt_orthonormal import *
+from models.mpt_mse import *
+from models.mpt_mse_no_const_term import *
+from models.mpt_original import *
+from models.mpt_traditional import *
+from models.mpt_orthonormal import *
 
 # OpenMP environment variables
 os.environ["MKL_NUM_THREADS"] = "1"  # always 1
@@ -114,12 +115,14 @@ class SCFT:
         # Multi-monomer polymer field theory
         print("---------------------------- MPT_MSE ----------------------------")
         self.mpt1 = MPT_MSE(self.monomer_types, self.chi_n)
+        print("---------------------------- MPT_MSE_No_Const ----------------------------")
+        self.mpt2 = MPT_MSE_No_Const(self.monomer_types, self.chi_n)
         print("---------------------------- MPT_Original ----------------------------")
-        self.mpt2 = MPT_Original(self.monomer_types, self.chi_n)
+        self.mpt3 = MPT_Original(self.monomer_types, self.chi_n)
         print("---------------------------- MPT_Traditional ----------------------------")
-        self.mpt3 = MPT_Traditional(self.monomer_types, self.chi_n)
+        self.mpt4 = MPT_Traditional(self.monomer_types, self.chi_n)
         print("---------------------------- MPT_Orthonormal ----------------------------")
-        self.mpt4 = MPT_Orthonormal(self.monomer_types, self.chi_n)
+        self.mpt5 = MPT_Orthonormal(self.monomer_types, self.chi_n)
         
         # Matrix for field residuals.
         # See *J. Chem. Phys.* **2017**, 146, 244902
@@ -139,6 +142,8 @@ class SCFT:
         self.matrix_p = np.identity(S) - np.matmul(np.ones((S,S)), matrix_pchip)/np.sum(matrix_chi_pinv)
         # print(matrix_chi)
         # print(matrix_chin)
+
+        self.non_zero_eigenvalues = self.mpt1.eigen_fields_real_idx + self.mpt1.eigen_fields_imag_idx
 
         # if phi_target is None:
         #     phi_target = np.ones(params["nx"])
@@ -288,9 +293,9 @@ class SCFT:
 
         # Total number of variables to be adjusted to minimize the Hamiltonian
         if params["box_is_altering"]:
-            n_var = len(self.monomer_types)*np.prod(params["nx"]) + len(params["lx"])
+            n_var = len(self.non_zero_eigenvalues)*np.prod(params["nx"]) + len(params["lx"])
         else :
-            n_var = len(self.monomer_types)*np.prod(params["nx"])
+            n_var = len(self.non_zero_eigenvalues)*np.prod(params["nx"])
             
         # Select an optimizer among 'Anderson Mixing' and 'ADAM' for finding saddle point        
         # (C++ class) Anderson Mixing method for finding saddle point
@@ -319,6 +324,12 @@ class SCFT:
             tolerance = params["tolerance"]
         else :
             tolerance = 1e-8     # Terminate iteration if the self-consistency error is less than tolerance
+
+        # dH/dw_eigen[i] is scaled by dt_scaling[i]
+        S = len(self.monomer_types)
+        self.dt_scaling = np.ones(S)
+        for i in range(S-1):
+            self.dt_scaling[i] = np.abs(self.mpt1.eigenvalues[i])/np.max(np.abs(self.mpt1.eigenvalues))
 
         # -------------- print simulation parameters ------------
         print("---------- Simulation Parameters ----------")
@@ -461,10 +472,85 @@ class SCFT:
         for i in range(S):
             w[i,:] = np.reshape(initial_fields[self.monomer_types[i]],  self.cb.get_n_grid())
 
-        # Keep the level of field value
-        for i in range(S):
-            w[i] -= self.cb.integral(w[i])/self.cb.get_volume()
-            
+        # # Keep the level of field value
+        # for i in range(S):
+        #     w[i] -= self.cb.integral(w[i])/self.cb.get_volume()
+
+        # w_eigen_1 = self.mpt1.to_eigen_fields(w)
+        # for i in range(S-1):
+        #     if np.isclose(self.mpt1.eigenvalues[i], 0.0):
+        #         w_eigen_1[i,:] = 0.0
+
+        # w_mpt2 = self.mpt2.to_monomer_fields(w_eigen_1)
+        # w_mpt3 = self.mpt3.to_monomer_fields(w_eigen_1)
+        # w_mpt4 = self.mpt4.to_monomer_fields(w_eigen_1)
+        # w_mpt5 = self.mpt5.to_monomer_fields(w_eigen_1)
+
+        # print("w_mpt2", np.mean(w_mpt2-w, axis=1), np.std(w_mpt2-w, axis=1))
+        # print("w_mpt3", np.mean(w_mpt3-w, axis=1), np.std(w_mpt3-w, axis=1))
+        # print("w_mpt4", np.mean(w_mpt4-w, axis=1), np.std(w_mpt4-w, axis=1))
+        # print("w_mpt5", np.mean(w_mpt5-w, axis=1), np.std(w_mpt5-w, axis=1))
+
+        # w_eigen_1 = self.mpt1.to_eigen_fields(w)
+        # w_eigen_1[1,:] = 0.0
+        # w = self.mpt1.to_monomer_fields(w_eigen_1)
+
+        # w_eigen_2 = self.mpt2.to_eigen_fields(w)
+        # for i in range(S-1):
+        #     if np.isclose(self.mpt2.eigenvalues[i], 0.0):
+        #         w_eigen_2[i,:] = -np.dot(self.mpt2.matrix_a_inv[i], self.mpt2.o_large_s)
+        # w = self.mpt2.to_monomer_fields(w_eigen_2)
+
+        # w_eigen_3 = self.mpt3.to_eigen_fields(w)
+        # for i in range(S-1):
+        #     if np.isclose(self.mpt3.eigenvalues[i], 0.0):
+        #         w_eigen_3[i,:] = -np.dot(self.mpt3.matrix_a_inv[i], self.mpt3.o_large_s)
+        # w = self.mpt3.to_monomer_fields(w_eigen_3)
+
+        # w_eigen_4 = self.mpt4.to_eigen_fields(w)
+        # for i in range(S-1):
+        #     if np.isclose(self.mpt4.eigenvalues[i], 0.0):
+        #         w_eigen_4[i,:] = 0.0
+        # w = self.mpt4.to_monomer_fields(w_eigen_4)
+
+        # w_eigen_5 = self.mpt5.to_eigen_fields(w)
+        # for i in range(S-1):
+        #     if np.isclose(self.mpt5.eigenvalues[i], 0.0):
+        #         w_eigen_5[i,:] = 0.0
+        # w = self.mpt5.to_monomer_fields(w_eigen_5)
+
+        # w_eigen_1 = self.mpt1.to_eigen_fields(w)
+        # w_eigen_4 = self.mpt4.to_eigen_fields(w)
+        # w_eigen_5 = self.mpt5.to_eigen_fields(w)
+
+        # print("w_eigen_1:\n", np.mean(w_eigen_1[1]), np.std(w_eigen_1[1]))
+        # print("w_eigen_4:\n", np.mean(w_eigen_4[1]), np.std(w_eigen_4[1]))
+        # print("w_eigen_5:\n", np.mean(w_eigen_5[1]), np.std(w_eigen_5[1]))
+
+        # for i in range(S-1):
+        #     if np.isclose(self.mpt1.eigenvalues[i], 0.0):
+        #         w_eigen_1[i,:] = 0.0
+        #     if np.isclose(self.mpt2.eigenvalues[i], 0.0):
+        #         w_eigen_2[i,:] = -np.dot(self.mpt2.matrix_a_inv[i], self.mpt2.o_large_s)
+        #     if np.isclose(self.mpt3.eigenvalues[i], 0.0):
+        #         w_eigen_3[i,:] = -np.dot(self.mpt3.matrix_a_inv[i], self.mpt3.o_large_s)
+        #     if np.isclose(self.mpt4.eigenvalues[i], 0.0):
+        #         w_eigen_4[i,:] = 0.0
+        #     if np.isclose(self.mpt5.eigenvalues[i], 0.0):
+        #         w_eigen_5[i,:] = 0.0
+        
+        # w_mpt1 = self.mpt1.to_monomer_fields(w_eigen_1)
+        # w_mpt2 = self.mpt2.to_monomer_fields(w_eigen_2)
+        # w_mpt3 = self.mpt3.to_monomer_fields(w_eigen_3)
+        # w_mpt4 = self.mpt4.to_monomer_fields(w_eigen_4)
+        # w_mpt5 = self.mpt5.to_monomer_fields(w_eigen_5)
+
+        # print("w_mpt1", np.mean(w_mpt1-w, axis=1), np.std(w_mpt1-w, axis=1))
+        # print("w_mpt2", np.mean(w_mpt2-w, axis=1), np.std(w_mpt2-w, axis=1))
+        # print("w_mpt3", np.mean(w_mpt3-w, axis=1), np.std(w_mpt3-w, axis=1))
+        # print("w_mpt4", np.mean(w_mpt4-w, axis=1), np.std(w_mpt4-w, axis=1))
+        # print("w_mpt5", np.mean(w_mpt5-w, axis=1), np.std(w_mpt5-w, axis=1))
+
         for scft_iter in range(1, self.max_iter+1):
 
             # Compute total concentration for each monomer type
@@ -479,24 +565,60 @@ class SCFT:
             w_eigen_2 = self.mpt2.to_eigen_fields(w)
             w_eigen_3 = self.mpt3.to_eigen_fields(w)
             w_eigen_4 = self.mpt4.to_eigen_fields(w)
-
-            # print("w_eigen_1:\n", np.mean(w_eigen_1[0]))
-            # print("w_eigen_2:\n", np.mean(w_eigen_2[0]))
-            # print("w_eigen_3:\n", np.mean(w_eigen_3[0]))
-            # print("w_eigen_4:\n", np.mean(w_eigen_4[0]))
-
-            # print("w_eigen_1**2:\n", np.mean(w_eigen_1[0]**2)*self.mpt1.h_coef_mu2[0])
-            # print("w_eigen_2**2:\n", np.mean(w_eigen_2[0]**2)*self.mpt2.h_coef_mu2[0])
-            # print("w_eigen_3**2:\n", np.mean(w_eigen_3[0]**2)*self.mpt3.h_coef_mu2[0])
-            # print("w_eigen_4**2:\n", np.mean(w_eigen_4[0]**2)*self.mpt4.h_coef_mu2[0])
+            w_eigen_5 = self.mpt5.to_eigen_fields(w)
 
             # Calculate the total energy
             # energy_total = - self.cb.integral(self.phi_target*w_eigen[S-1])/self.cb.get_volume()
             total_partitions = [self.solver.get_total_partition(p) for p in range(self.molecules.get_n_polymer_types())]
+            
+            # Compute Hamiltonian part that total partition functions
+            energy_total_0 = 0.0
+            for p in range(self.molecules.get_n_polymer_types()):
+                energy_total_0 -= self.molecules.get_polymer(p).get_volume_fraction()/ \
+                                self.molecules.get_polymer(p).get_alpha() * \
+                                np.log(total_partitions[p])
+            for key in self.chi_n:
+                monomer_pair = sorted(key.split(","))
+                energy_total_0 += np.mean(phi[monomer_pair[0]]*phi[monomer_pair[1]]*self.chi_n[key])
+            for count, type in enumerate(self.monomer_types):
+                energy_total_0 -= np.mean(w[count]*phi[type])
+
             energy_total_1 = self.mpt1.compute_hamiltonian(self.molecules, w_eigen_1, total_partitions)
             energy_total_2 = self.mpt2.compute_hamiltonian(self.molecules, w_eigen_2, total_partitions)
             energy_total_3 = self.mpt3.compute_hamiltonian(self.molecules, w_eigen_3, total_partitions)
             energy_total_4 = self.mpt4.compute_hamiltonian(self.molecules, w_eigen_4, total_partitions)
+            energy_total_5 = self.mpt5.compute_hamiltonian(self.molecules, w_eigen_5, total_partitions)
+
+            # # Compute functional derivatives of Hamiltonian w.r.t. imaginary eigen fields 
+            # h_deriv, _ = self.mpt1.compute_func_deriv(w_eigen_1, phi, self.non_zero_eigenvalues)
+
+            # # Compute total error
+            # old_error_level = error_level
+            # error_level_array = np.std(h_deriv, axis=1)
+            # error_level = np.max(error_level_array)
+
+            # # Print iteration # and error levels and check the mass conservation
+            # mass_error = np.mean(h_deriv[-1])
+            
+            # print("%8d %12.3E " % (scft_iter, mass_error), end=" [ ")
+            # for p in range(self.molecules.get_n_polymer_types()):
+            #     print("%13.7E " % (self.solver.get_total_partition(p)), end=" ")
+            # print("] [%12.9f, %12.9f, %12.9f, %12.9f, %12.9f, %12.9f] %15.7E " 
+            #         % (energy_total_0, energy_total_1, energy_total_2, energy_total_3, energy_total_4, energy_total_5, error_level))
+
+            # # Conditions to end the iteration
+            # if error_level < self.tolerance:
+            #     break
+
+            # # Scaling h_deriv
+            # for count, i in enumerate(self.non_zero_eigenvalues):
+            #     h_deriv[count] *= self.dt_scaling[i]
+
+            # # Calculate new fields using simple and Anderson mixing
+            # w_eigen_1[self.non_zero_eigenvalues] = np.reshape(self.field_optimizer.calculate_new_fields(w_eigen_1[self.non_zero_eigenvalues], -h_deriv, old_error_level, error_level), [len(self.non_zero_eigenvalues), self.cb.get_n_grid()])
+
+            # # Convert eigen fields to monomer fields
+            # w = self.mpt1.to_monomer_fields(w_eigen_1)
 
             # Calculate difference between current total density and target density
             phi_total = np.zeros(self.cb.get_n_grid())
@@ -504,6 +626,9 @@ class SCFT:
                 phi_total += phi[self.monomer_types[i]]
             # phi_diff = phi_total-self.phi_target
             phi_diff = phi_total-1.0
+
+            # Compute functional derivatives of Hamiltonian w.r.t. imaginary eigen fields 
+            w_diff, _ = self.mpt1.compute_func_deriv(w_eigen_1, phi, self.non_zero_eigenvalues)
 
             # Calculate self-consistency error
             w_diff = np.zeros([S, self.cb.get_n_grid()], dtype=np.float64) # array for output fields
@@ -523,7 +648,7 @@ class SCFT:
             error_normal = 1.0  # add 1.0 to prevent divergence
             for i in range(S):
                 error_level += self.cb.inner_product(w_diff[i],w_diff[i])
-                error_normal += self.cb.inner_product(w[i],w[i])
+                error_normal += self.cb.inner_product(w_eigen_1[i],w_eigen_1[i])
             error_level = np.sqrt(error_level/error_normal)
 
             # Print iteration # and error levels and check the mass conservation
@@ -539,15 +664,15 @@ class SCFT:
                 (scft_iter, mass_error), end=" [ ")
                 for p in range(self.molecules.get_n_polymer_types()):
                     print("%13.7E " % (self.solver.get_total_partition(p)), end=" ")
-                print("] [%12.9f, %12.9f, %12.9f, %12.9f] %15.7E " 
-                      % (energy_total_1, energy_total_2, energy_total_3, energy_total_4, error_level), end=" ")
+                print("] [%12.9f, %12.9f, %12.9f, %12.9f, %12.9f, %12.9f] %15.7E " 
+                      % (energy_total_0, energy_total_1, energy_total_2, energy_total_3, energy_total_4, energy_total_5, error_level), end=" ")
                 print("[", ",".join(["%10.7f" % (x) for x in self.cb.get_lx()]), "]")
             else:
                 print("%8d %12.3E " % (scft_iter, mass_error), end=" [ ")
                 for p in range(self.molecules.get_n_polymer_types()):
                     print("%13.7E " % (self.solver.get_total_partition(p)), end=" ")
-                print("] [%12.9f, %12.9f, %12.9f, %12.9f] %15.7E " 
-                      % (energy_total_1, energy_total_2, energy_total_3, energy_total_4, error_level))
+                print("] [%12.9f, %12.9f, %12.9f, %12.9f, %12.9f, %12.9f] %15.7E " 
+                      % (energy_total_0, energy_total_1, energy_total_2, energy_total_3, energy_total_4, energy_total_5, error_level))
 
             # Conditions to end the iteration
             if error_level < self.tolerance:
@@ -594,6 +719,16 @@ class SCFT:
         # Store phi and w
         self.phi = phi
         self.w = w
+
+        # w_eigen_1 = self.mpt1.to_eigen_fields(w)
+        # w_eigen_4 = self.mpt4.to_eigen_fields(w)
+        # w_eigen_5 = self.mpt5.to_eigen_fields(w)
+
+        # print("w_eigen_1:\n", np.mean(w_eigen_1[1]), np.std(w_eigen_1[1]))
+        # print("w_eigen_4:\n", np.mean(w_eigen_4[1]), np.std(w_eigen_4[1]))
+        # print("w_eigen_5:\n", np.mean(w_eigen_5[1]), np.std(w_eigen_5[1]))
+
+        # print("phi['A'][0:32]:", phi["A"][0:32])
 
     # def get_concentrations(self,):
     #     return self.phi