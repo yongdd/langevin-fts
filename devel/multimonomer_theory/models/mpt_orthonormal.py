bug fix: wrong the sign of dH/dW

devel/multimonomer_theory/models/mpt_orthonormal.py
@@ -1,7 +1,7 @@
 import time
 import numpy as np
 
-class MPT_Original:
+class MPT_Orthonormal:
     def __init__(self, monomer_types, chi_n):
         self.monomer_types = monomer_types
         S = len(self.monomer_types)
@@ -26,10 +26,6 @@ class MPT_Original:
         self.matrix_chi = matrix_chi
         self.vector_s = np.matmul(matrix_chi, np.ones(S))/S
         self.vector_large_s = np.matmul(np.transpose(matrix_o), self.vector_s)
-
-        vector_large_s_prime = self.vector_large_s.copy()
-        vector_large_s_prime[S-1] = 0.0
-        self.o_large_s = np.reshape(np.matmul(matrix_o, vector_large_s_prime), (S, 1))/S
 
         # Indices whose eigen fields are real
         self.eigen_fields_real_idx = []
@@ -59,8 +55,8 @@ class MPT_Original:
 
         # Matrix A and Inverse for converting between eigen fields and species chemical potential fields
         matrix_a = matrix_o.copy()
-        matrix_a_inv = np.transpose(matrix_o).copy()/S
-        
+        matrix_a_inv = np.transpose(matrix_o).copy()
+
         # Check the inverse matrix
         error = np.std(np.matmul(matrix_a, matrix_a_inv) - np.identity(S))
         assert(np.isclose(error, 0.0)), \
@@ -117,18 +113,19 @@ class MPT_Original:
         print("\tcoefficients of int of mu(r)/V: ", self.h_coef_mu1)
         print("\tcoefficients of int of mu(r)^2/V: ", self.h_coef_mu2)
         print("\tdH_ref/dχN: ", self.h_const_deriv_chin)
-        print("\td(coef of mu1)/dχN: ", self.h_coef_mu1_deriv_chin)
-        print("\td(coef of mu2)/dχN: ", self.h_coef_mu2_deriv_chin)
+        print("\td(coef of mu(r))/dχN: ", self.h_coef_mu1_deriv_chin)
+        print("\td(coef of mu(r)^2)/dχN: ", self.h_coef_mu2_deriv_chin)
 
     def to_eigen_fields(self, w):
-        return np.matmul(self.matrix_a_inv, w-self.o_large_s)
+        return np.matmul(self.matrix_a_inv, w)
 
     def to_monomer_fields(self, w_eigen):
-        return np.matmul(self.matrix_a, w_eigen) + self.o_large_s
+        return np.matmul(self.matrix_a, w_eigen)
 
     def compute_eigen_system(self, chi_n, matrix_p):
         S = matrix_p.shape[0]
 
+        # Compute eigenvalues and eigenvectors
         matrix_chi = np.zeros((S,S))
         for i in range(S):
             for j in range(i+1,S):
@@ -139,13 +136,14 @@ class MPT_Original:
                     matrix_chi[i,j] = chi_n[key]
                     matrix_chi[j,i] = chi_n[key]
         projected_chin = np.matmul(matrix_p, np.matmul(matrix_chi, matrix_p))
-
         eigenvalues, eigenvectors = np.linalg.eigh(projected_chin)
+
+        # Reordering eigenvalues and eigenvectors
         sorted_indexes = np.argsort(np.abs(eigenvalues))[::-1]
         eigenvalues = eigenvalues[sorted_indexes]
         eigenvectors = eigenvectors[:,sorted_indexes]
 
-        # Set the last eigenvector to np.ones(S)/np.sqrt(S)
+        # Set the last eigenvector to [1, 1, ..., 1]/√S
         eigenvectors[:,-1] = np.ones(S)/np.sqrt(S)
         
         # Make a orthogonal matrix using Gram-Schmidt
@@ -164,25 +162,39 @@ class MPT_Original:
             if eigenvectors[0,i] < 0.0:
                 eigenvectors[:,i] *= -1.0
 
-        # Multiply np.sqrt(S) to eigenvectors
-        eigenvectors *= np.sqrt(S)
+        # # Multiply √S to eigenvectors
+        # eigenvectors *= np.sqrt(S)
 
         return eigenvalues, eigenvectors
 
     def compute_h_coef(self, chi_n, eigenvalues):
         S = len(self.monomer_types)
 
+        # Compute vector X_iS
+        vector_s = np.zeros(S-1)
+        for i in range(S-1):
+            monomer_pair = [self.monomer_types[i], self.monomer_types[S-1]]
+            monomer_pair.sort()
+            key = monomer_pair[0] + "," + monomer_pair[1]            
+            vector_s[i] = chi_n[key]
+
         # Compute reference part of Hamiltonian
         h_const = 0.5*np.sum(self.vector_s)/S
+        for i in range(S-1):
+            if not np.isclose(eigenvalues[i], 0.0):
+                h_const -= 0.5*self.vector_large_s[i]**2/eigenvalues[i]
 
         # Compute coefficients of integral of μ(r)/V
         h_coef_mu1 = np.zeros(S-1)
+        for i in range(S-1):
+            if not np.isclose(eigenvalues[i], 0.0):
+                h_coef_mu1[i] = self.vector_large_s[i]/eigenvalues[i]
 
         # Compute coefficients of integral of μ(r)^2/V
         h_coef_mu2 = np.zeros(S-1)
         for i in range(S-1):
             if not np.isclose(eigenvalues[i], 0.0):
-                h_coef_mu2[i] = -0.5/eigenvalues[i]*S
+                h_coef_mu2[i] = -0.5/eigenvalues[i]
 
         return h_const, h_coef_mu1, h_coef_mu2
 
@@ -191,7 +203,7 @@ class MPT_Original:
         S = len(self.monomer_types)
 
         # Compute Hamiltonian part that is related to fields
-        hamiltonian_fields = -np.mean(w_eigen[S-1])
+        hamiltonian_fields = -np.mean(w_eigen[S-1])/np.sqrt(S)
         for i in range(S-1):
             hamiltonian_fields += self.h_coef_mu2[i]*np.mean(w_eigen[i]**2)
             hamiltonian_fields += self.h_coef_mu1[i]*np.mean(w_eigen[i])
@@ -219,12 +231,11 @@ class MPT_Original:
                 h_deriv[count] +=   self.h_coef_mu1[i]
                 for j in range(S):
                     h_deriv[count] += self.matrix_a[j,i]*phi[self.monomer_types[j]]
-                
-            # Pressure fields
+            # Pressure field
             else:
                 for j in range(S):
-                    h_deriv[count] += phi[self.monomer_types[j]]
-                h_deriv[count] -= 1.0
+                    h_deriv[count] -= phi[self.monomer_types[j]]
+                h_deriv[count] += 1.0
         elapsed_time["h_deriv"] = time.time() - time_e_start
         
         return  h_deriv, elapsed_time

devel/multimonomer_theory/scft/ABC_Triblock_Sphere3D.py
@@ -10,7 +10,7 @@ os.environ["OMP_MAX_ACTIVE_LEVELS"] = "2"  # 0, 1 or 2
 os.environ["OMP_NUM_THREADS"] = "2"  # 1 ~ 4
 
 # Major Simulation params
-chin = 9.5        # Interaction parameter, Flory-Huggins params * N_Ref
+chin = 12.5        # Interaction parameter, Flory-Huggins params * N_Ref
 
 params = {
     # "platform":"cuda",           # choose platform among [cuda, cpu-mkl]
@@ -74,9 +74,12 @@ calculation = scft.SCFT(params=params)
 #     w_A[molecules,my,mz] = -50/(np.prod(params["lx"])/np.prod(params["nx"]))
 # w_A = gaussian_filter(w_A, sigma=np.min(params["nx"])/5, mode='wrap')
 
-w_A = np.random.normal(0.0, 5.0, params["nx"])
-w_B = np.random.normal(0.0, 5.0, params["nx"])
-w_C = np.random.normal(0.0, 5.0, params["nx"])
+# random_seed = 12345
+# np.random.seed(random_seed)
+
+w_A = np.random.normal(0.0, 5.0, params["nx"]) + 0.5
+w_B = np.random.normal(0.0, 5.0, params["nx"]) + 1.0
+w_C = np.random.normal(0.0, 5.0, params["nx"]) + 1.5
 
 # Set a timer
 time_start = time.time()
@@ -90,10 +93,3 @@ print("total time: %f " % time_duration)
 
 # Save final results
 calculation.save_results("fields.mat")
-
-# Recording first a few iteration results for debugging and refactoring
-    #    1    0.000E+00  [ 3.6079092E+00  ]    -0.318161996   4.4693432E+00  [  2.9000000, 2.9000000, 2.9000000 ]
-    #    2    1.665E-15  [ 3.0252432E+00  ]    -0.249220685   3.8560277E+00  [  2.9006622, 2.9006622, 2.9006622 ]
-    #    3    1.998E-15  [ 2.6847016E+00  ]    -0.201835726   3.3958896E+00  [  2.9011100, 2.9011100, 2.9011100 ]
-    #    4   -1.971E-15  [ 2.4628496E+00  ]    -0.166892626   3.0245843E+00  [  2.9014276, 2.9014276, 2.9014276 ]
-    #    5    4.441E-16  [ 2.3083187E+00  ]    -0.139935578   2.7121688E+00  [  2.9016589, 2.9016589, 2.9016589 ]